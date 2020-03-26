686914f95ff05117479e33084d701de9849c1774
Toby Smith <tobysmith568@hotmail.co.uk>
2020-03-26 17:30:04 +0000

    Added a pull script

---
+++ pull.ts
@@ -0,0 +1,14 @@
+import { Script, IScript, IResult, IRepository } from "d:/Users/tobys/Repositories/University/Fibre/StandardScripts/node_modules/@fibre/types/dist/index";
+
+@Script("pull")
+export class Pull implements IScript {
+	
+	public async run(repositories: IRepository[]): Promise<IResult> {
+		
+		for (const repository of repositories) {
+			await repository.pull();
+		}
+
+		return { success: true };
+	}
+}
