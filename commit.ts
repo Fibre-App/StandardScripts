4851d1805052e191356cdabb1ab38ddedd28d578
Toby Smith <tobysmith568@hotmail.co.uk>
2020-04-02 02:06:46 +0100

    Bumped the types version

--- commit.ts
+++ commit.ts
@@ -1,12 +1,15 @@
-import { Script, IScript, IResult, IRepository } from "d:/Users/tobys/Repositories/University/Fibre/StandardScripts/node_modules/@fibre/types/dist/index";
+import { Script, IScript, IResult, IRepository, Inject, A, ILogger } from "d:/Users/tobys/Repositories/University/Fibre/StandardScripts/node_modules/@fibre/types/dist/index";
 
 @Script("commit")
 export class commit implements IScript {
+
+	constructor(@Inject(A.Logger) private readonly logger: ILogger) { }
 	
 	public async run(repositories: IRepository[]): Promise<IResult> {
 
 		const message = "This is a commit message";// TODO Needs to come from dialog service
 		
+		this.logger.info("FROM SCRIPT: ABOUT TO COMMIT!");
 		for (const repository of repositories) {
 			await repository.commit(message);
 		}
