71c4a2ea080955cd797b12fb7124aaa16f226f96
Toby Smith <tobysmith568@hotmail.co.uk>
2020-04-03 12:45:41 +0100

    Cleaned up the commit script

--- commit.ts
+++ commit.ts
@@ -1,17 +1,14 @@
-import { Script, IScript, IResult, IRepository, Inject, A, ILogger, IToasterService } from "d:/Users/tobys/Repositories/University/Fibre/StandardScripts/node_modules/@fibre/types/dist/index";
+import { Script, IScript, IResult, IRepository, Inject, A, IDialogService } from "d:/Users/tobys/Repositories/University/Fibre/StandardScripts/node_modules/@fibre/types/dist/index";
 
 @Script("commit")
 export class commit implements IScript {
 
-	constructor(@Inject(A.Logger) private readonly logger: ILogger,
-							@Inject(A.ToasterService) private readonly toaster: IToasterService) { }
+	constructor(@Inject(A.DialogService) private readonly dialogService: IDialogService) { }
 	
 	public async run(repositories: IRepository[]): Promise<IResult> {
 
-		const message = "This is a commit message";// TODO Needs to come from dialog service
-		
-		this.logger.info("FROM SCRIPT: ABOUT TO COMMIT!");
-		this.toaster.information("TITLE!", "About to commit!")
+		const message = await this.dialogService.openSingleLineDialog("Committing", "Enter a commit message", "WIP");
+
 		for (const repository of repositories) {
 			await repository.commit(message);
 		}
